9c588042216addb83f1c9ea58724fbf162ded5e3
nina2cool <christinalmcmillan@gmail.com>
2016-11-10 18:57:07 -0600

    first pass at contact list app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import ContactList from './ContactList';
+import Contact from './Contact';
+import SearchBar from './SearchBar';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Contact List!</h1>
+        <SearchBar />
+        <ContactList />
       </div>
     );
   }
